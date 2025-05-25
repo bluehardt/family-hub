6628b67a2106f3134acb3ab33d9a24c12bee5903
bluehardt <finger.pointing.objection@gmail.com>
2025-05-25 21:51:04 +0200

    adjusting vercel.json

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,9 @@
 
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
-import '@angular/localize/init';
-import { LOCALE_ID } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
-import localePl from '@angular/common/locales/pl';
-import localePlExtra from '@angular/common/locales/extra/pl';
-
-// registerLocaleData(localePl, 'it-IT', localePlExtra);
 
 platformBrowser()
   .bootstrapModule(AppModule, {
     ngZoneEventCoalescing: true,
-    // providers: [{ provide: LOCALE_ID, useValue: 'pl-PL' }],
   })
   .catch((err) => console.error(err));
